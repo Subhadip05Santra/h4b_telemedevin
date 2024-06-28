7e2a9b007b43423c400613cd06f551c0b92e3df4
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-06-28 21:29:35 +0530

    Added index for components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Accordion } from "./landing/Accordion.component";
+export { default as ContainerScroll } from "./landing/container-scroll-animation";
+export { default as Vortex } from "./landing/vortex";
+export { default as Navbar } from "./landing/Navbar.component";
+export { default as Footer } from "./landing/Footer.component";
+export { default as Feature } from "./landing/Feature.component";
