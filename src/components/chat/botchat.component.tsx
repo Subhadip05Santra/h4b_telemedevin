61620d4f0988151691fffd0242e0d962bea936dc
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-06-29 01:47:03 +0530

    Added Components and files for Chat Page

---
+++ src/components/chat/botchat.component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BotChat = () => {
+  return (
+    <div>BotChat</div>
+  )
+}
+
+export default BotChat
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,11 @@
+// Landing Page Imports
 export { default as Accordion } from "./landing/Accordion.component";
 export { default as ContainerScroll } from "./landing/container-scroll-animation";
 export { default as Vortex } from "./landing/vortex";
 export { default as Navbar } from "./landing/Navbar.component";
 export { default as Footer } from "./landing/Footer.component";
 export { default as Feature } from "./landing/Feature.component";
+
+//  Chat Components Components
+export { default as BotChat } from "./chat/botchat.component";
+export { default as UserChat } from "./chat/userchat.component";
