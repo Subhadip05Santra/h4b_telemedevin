12480cff80c2e1abf8e3c72f404b1e5aa6bdc1a0
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-06-29 14:21:18 +0530

    Updatted coponents

---
+++ src/app/(page)/video/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoFeat = () => {
+  return (
+    <div>VideoFeat</div>
+  )
+}
+
+export default VideoFeat
--- src/components/chat/botchat.component.tsx
+++ src/components/chat/botchat.component.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 
-const BotChat = () => {
-  return (
-    <div>
-      <div className="flex items-start gap-3">
-        <Avatar className="h-8 w-8 border">
-          <AvatarFallback>BOT</AvatarFallback>
-        </Avatar>
-        <div className="max-w-[75%] rounded-lg bg-muted p-3 text-sm">
-          <p>Hey, how's it going?</p>
-          <div className="mt-2 text-xs text-muted-foreground">2:34 PM</div>
+const BotChat = () =>
+  // props: any
+  {
+    return (
+      <div>
+        <div className="flex items-start gap-3">
+          <Avatar className="h-8 w-8 border">
+            <AvatarFallback>
+              {/* {props.boticon === "" ? "BO" : props.boticon} */}
+              BO
+            </AvatarFallback>
+          </Avatar>
+          <div className="max-w-[75%] rounded-lg bg-muted p-3 text-sm">
+            <p>
+              {/* {props.botreply === "" ? "This is a Bot Response." : props.botreply} */}
+              This is a Bot Response.
+            </p>
+            <div className="mt-2 text-xs text-muted-foreground">
+              {/* {props.bottime === "" ? "12:00" : props.bottime} */}
+              12:00
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default BotChat;
